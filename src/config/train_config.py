f409c2003c7d4ef6b966a9cb082fc36ae6d6e17b
Sofiia Kashperova <47923051+kashperova@users.noreply.github.com>
2024-10-12 20:35:45 +0300

    Fix configs (#7)
    * Fix noisy student trainer
    
    * Minor
    
    * Fix configs
    
    ---------
    
    Co-authored-by: Sofiia Kashperova <@kashperova>

--- src/config/train_config.py
+++ src/config/train_config.py
@@ -11,15 +11,3 @@ class BaseTrainConfig:
     def params(self) -> Dict[str, Any]:
         attrs = set(self.__class__.__dict__.keys()) - set(self.__dict__.keys())
         return {attr: getattr(self, attr) for attr in attrs}
-
-
-class SLConfig(BaseTrainConfig):
-    teacher: BaseTrainConfig = None
-    student: BaseTrainConfig = None
-
-    def set(self, teacher: Dict[str, Any], student: Dict[str, Any]) -> None:
-        for k, v in teacher.items():
-            setattr(self.teacher, k, v)
-
-        for k, v in student.items():
-            setattr(self.student, k, v)
