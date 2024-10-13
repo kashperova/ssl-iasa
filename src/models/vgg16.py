57109fd35cafad659b9e9331c17229f73db28043
Sofiia Kashperova <47923051+kashperova@users.noreply.github.com>
2024-10-13 10:49:17 +0300

    Fix/noisy student trainer (#9)
    * Fix noisy student trainer
    
    * Minor
    
    * Fix configs
    
    * Add flag for teacher training
    
    * Add shapes check to annotator & update vgg16
    
    ---------
    
    Co-authored-by: Sofiia Kashperova <@kashperova>

--- src/models/vgg16.py
+++ src/models/vgg16.py
@@ -1,4 +1,3 @@
-import random
 from typing import Optional
 
 import torch
@@ -11,12 +10,10 @@ class VGG16(nn.Module):
         self,
         num_classes: int,
         dropout: Optional[float] = 0.5,
-        st_depth_prob: Optional[float] = 0.5,
         pretrained: Optional[bool] = True,
     ):
         super(VGG16, self).__init__()
         self.vgg16 = models.vgg16(pretrained=pretrained)
-        self.st_depth_prob = st_depth_prob
 
         self.vgg16.classifier = nn.Sequential(
             nn.Linear(25088, 4096),
@@ -28,20 +25,11 @@ class VGG16(nn.Module):
             nn.Linear(4096, num_classes)
         )
 
-    def stochastic_depth(self, x, layer):
-        if self.training and random.uniform(0, 1) < self.st_depth_prob:
-            return x
-        else:
-            return layer(x)
-
     def forward(self, x):
         x = self.vgg16.features(x)
         x = torch.flatten(x, 1)
 
         for i, layer in enumerate(self.vgg16.classifier):
-            if isinstance(layer, nn.Linear):
-                x = self.stochastic_depth(x, layer)
-            else:
-                x = layer(x)
+            x = layer(x)
 
         return x
